e1e690bf3bfb73840d4e23e0c5210700819c6135
Yao Li <hnkfliyao@gmail.com>
2017-01-03 22:47:13 -0500

    Initial Commit

---
+++ myhttpd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  myhttpd
+//
+//  Created by lastland on 03/01/2017.
+//  Copyright © 2017 DeepSpec. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
